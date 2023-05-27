6040840bad2e892774f617ccf4be6905c46e68dd
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-05-27 13:56:30 +0530

    fix: react import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import  React, { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Test ur </h1>
+      <h1>Test </h1>
     </>
   );
 }
