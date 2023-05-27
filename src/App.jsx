41ad7cf18806093fa9cce143fa7ee7a0812745d7
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-05-27 15:07:00 +0530

    chore: created day card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import useAllowanceStore from "./store/useAllowanceStore";
+import DayCard from "./components/DayCard";
 
 function App() {
-  const { allowance, setAllowance } = useAllowanceStore();
+  const { allowance } = useAllowanceStore();
+
+  const renderDays = () => {
+    const days = [];
+    for (let i = 1; i <= 30; i++) {
+      days.push(<DayCard key={i} no={i} limit={limit} />);
+    }
+    return days;
+  };
 
   const calcLimit = () =>  {
     setLimit(allowance / 30);
@@ -15,7 +24,7 @@ function App() {
   return (
     <>
       <Input calc={calcLimit}></Input>
-      <div>{limit}</div>
+      <div>{renderDays()}</div>
     </>
   );
 }
